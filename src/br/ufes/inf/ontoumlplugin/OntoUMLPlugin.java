c48832762ad0e66fda3792709ac7e6e2cbdc48c2
Marcus Sales <old.marcussales@gmail.com>
2017-05-23 22:19:51 -0300

    Semente do projeto plugin OntoUML para Visual Paradigm

---
+++ src/br/ufes/inf/ontoumlplugin/OntoUMLPlugin.java
@@ -0,0 +1,20 @@
+package br.ufes.inf.ontoumlplugin;
+
+import com.vp.plugin.VPPlugin;
+import com.vp.plugin.VPPluginInfo;
+
+public class OntoUMLPlugin implements VPPlugin {
+
+	@Override
+	public void loaded(VPPluginInfo arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void unloaded() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
