55e0eb3d214e5c97297c4f79d9d68a05795b0990
Marcus Sales <old.marcussales@gmail.com>
2017-09-02 12:29:35 -0300

    Desenho de diagrama carregado por arquivo refontouml

--- src/br/ufes/inf/ontoumlplugin/actions/LoadOntoUMLModelController.java
+++ src/br/ufes/inf/ontoumlplugin/actions/LoadOntoUMLModelController.java
@@ -2,7 +2,9 @@ package br.ufes.inf.ontoumlplugin.actions;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 
 import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileNameExtensionFilter;
@@ -10,10 +12,19 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 import org.eclipse.emf.ecore.resource.Resource;
 
 import com.vp.plugin.ApplicationManager;
+import com.vp.plugin.DiagramManager;
 import com.vp.plugin.action.VPAction;
 import com.vp.plugin.action.VPActionController;
+import com.vp.plugin.diagram.IClassDiagramUIModel;
+import com.vp.plugin.diagram.IDiagramTypeConstants;
 import com.vp.plugin.diagram.IDiagramUIModel;
+import com.vp.plugin.diagram.shape.IClassUIModel;
+import com.vp.plugin.diagram.shape.IPackageUIModel;
+import com.vp.plugin.model.IClass;
+import com.vp.plugin.model.IModelElement;
+import com.vp.plugin.model.IPackage;
 import com.vp.plugin.model.IProject;
+import com.vp.plugin.model.factory.IModelElementFactory;
 
 import RefOntoUML.Classifier;
 import RefOntoUML.Element;
@@ -23,6 +34,12 @@ import RefOntoUML.parser.OntoUMLParser;
 import RefOntoUML.util.RefOntoUMLResourceUtil;
 
 public class LoadOntoUMLModelController implements VPActionController {
+	
+	private Map<RefOntoUML.Classifier, IModelElement> ontoUml2VpClasses;
+	
+	public LoadOntoUMLModelController(){
+		this.ontoUml2VpClasses = new HashMap<>();
+	}
 
 	@Override
 	public void performAction(VPAction arg0) {
@@ -34,15 +51,14 @@ public class LoadOntoUMLModelController implements VPActionController {
 				.getProject();
 
 
-		IDiagramUIModel diagram = ApplicationManager
-		                        .instance()
-		                        .getDiagramManager()
-		                        .getActiveDiagram();
+		DiagramManager diagramManager = ApplicationManager
+				                        .instance()
+				                        .getDiagramManager();
 		
 		JFileChooser fileChooser = ApplicationManager.instance().getViewManager().createJFileChooser();
 		FileNameExtensionFilter filter = new FileNameExtensionFilter("Reference OntoUML (*.refontouml)", "refontouml");
 		fileChooser.setFileFilter(filter);
-		fileChooser.setDialogTitle("Selecione o arquivo refontouml");
+		fileChooser.setDialogTitle("Selecione o arquivo RefOntoUML");
 		fileChooser.setDialogType(JFileChooser.OPEN_DIALOG);
 		int returnValue = fileChooser.showOpenDialog(null);
 		
@@ -50,13 +66,39 @@ public class LoadOntoUMLModelController implements VPActionController {
 			File file = fileChooser.getSelectedFile();
 			try {
 				Resource model = RefOntoUMLResourceUtil.loadModel(file.getAbsolutePath());
+				IClassDiagramUIModel diagram = 
+						(IClassDiagramUIModel) diagramManager.createDiagram(IDiagramTypeConstants.DIAGRAM_TYPE_CLASS_DIAGRAM);
+				
 				RefOntoUML.Package ontoUmlPackage = (RefOntoUML.Package) model.getContents().get(0);
-				System.out.println(ontoUmlPackage.toString());
 				OntoUMLParser parser = new OntoUMLParser(ontoUmlPackage);
+				
 				for(Classifier c : parser.getRigidClasses()){
-					System.out.println(c.getName());
+					IClass vpClass = IModelElementFactory.instance().createClass();
+					vpClass.setName(c.getName());
+					this.ontoUml2VpClasses.put(c, vpClass);
+					
+					// create superclass shape
+					IClassUIModel vpClassUi = (IClassUIModel) diagramManager.createDiagramElement(diagram, vpClass);
+					vpClassUi.setRequestResetCaption(true);
 				}
-				//RefOntoUMLResourceUtil.saveModel(new File("/home/mvp-sales/Documentos/pqp.refontouml").getAbsolutePath(), ontoUmlPackage);
+				
+				for(Classifier c : parser.getAntiRigidClasses()){
+					IClass vpClass = IModelElementFactory.instance().createClass();
+					vpClass.setName(c.getName());
+					this.ontoUml2VpClasses.put(c, vpClass);
+					
+					// create superclass shape
+					IClassUIModel vpClassUi = (IClassUIModel) diagramManager.createDiagramElement(diagram, vpClass);
+					vpClassUi.setRequestResetCaption(true);
+				}
+				
+				for(Classifier c : parser.getAssociations()){
+					RefOntoUML.Association a = (RefOntoUML.Association) c;
+				}
+				
+				diagramManager.layout(diagram, DiagramManager.LAYOUT_AUTO);
+				diagramManager.openDiagram(diagram);
+				
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
